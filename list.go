d0078dbc17182fe33cb9ac72106f24fb18dcd2b4
CarlosChicata <carlos.fernando.chicata@gmail.com>
2021-02-15 21:57:57 -0500

    feat: create a linked list with append service

--- list.go
+++ list.go
@@ -2,6 +2,43 @@ package main
 
 import "fmt"
 
+
+type myNode struct {
+	value string
+	next *myNode
+}
+
+type myLinkedList struct {
+	length int
+	head *myNode
+}
+
+func (l *myLinkedList) len() int {
+	return l.length
+}
+
+func (L *myLinkedList) append(data string) {
+	newMyNode := &myNode{value: data, next: nil }
+
+	if L.head == nil {
+		fmt.Println("first time")
+		L.head = newMyNode
+	}else{
+		currentPointer := L.head
+		for currentPointer.next != nil {
+			currentPointer = currentPointer.next
+		}
+		currentPointer.next = newMyNode
+	}
+	L.length++;
+}
+
+
 func main(){
-	fmt.Println("Hola Mundo!")
+	testingList := myLinkedList{length: 0}
+	testingList.append("hola")
+	testingList.append("que")
+	testingList.append("tal?")
+	fmt.Println(testingList.len())
+	fmt.Println(testingList.head)
 }
