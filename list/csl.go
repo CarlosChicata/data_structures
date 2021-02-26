7d898fd183eb339029eb14c996b84616f162be61
CarlosChicata <carlos.fernando.chicata@gmail.com>
2021-02-26 07:39:53 -0500

    docs: update docstring of csl file

--- list/csl.go
+++ list/csl.go
@@ -10,7 +10,7 @@ Methods:
 	- display [x] : display element in list
 	- popend [x] : remove  last element of list
 	- appstart [x] : add element to first in list
-	- popstart [ ] : remove element to first in list
+	- popstart [x] : remove element to first in list
 	- insert [ ] : add element into list based in position if apply
 	- pop [ ] : remove element in list based in position
 */
