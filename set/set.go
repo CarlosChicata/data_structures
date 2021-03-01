1f5fb73f15fbf4ab2fd0c7c85ce0f14e3729878c
CarlosChicata <carlos.fernando.chicata@gmail.com>
2021-03-01 05:38:42 -0500

    feat: add belong, display and add method into set

--- set/set.go
+++ set/set.go
@@ -11,7 +11,7 @@ Methods:
 	- belong [ ] : element belong this set
 	- difference [ ] : all elemnet will not interset in two sets
 	- len [ ] : count all elements of universe set
-	- display [] : display all element in universe set
+	- display [ ] : display all element in universe set
 */
 
 package main
@@ -19,12 +19,52 @@ package main
 import "fmt"
 
 
-type uniqueSet {
-	
+type uniqueSet struct {
+	universe []string
 }
 
 
+func (S *uniqueSet) belong (element string) bool {
+	isBelong := false
+	for _, value := range S.universe {
+		if element == value {
+			isBelong = true
+			break;
+		}
+	}
+	return isBelong
+}
+
+func (S *uniqueSet) display () {
+	var msg string
+	for _, value := range S.universe {
+		msg = fmt.Sprintf("value is: %s", value)
+		fmt.Println(msg)
+	}
+	fmt.Println("------END------")
+}
+
+func (S *uniqueSet) add (element string) bool {
+	var isBelong bool;
+	isBelong = S.belong(element)
+
+	if isBelong {
+		isBelong = false
+	}else{
+		S.universe = append(S.universe, element)
+		isBelong = true
+	}
+	return isBelong
+}
 
 
 func main(){
+	testingSet := uniqueSet{}
+	testingSet.add("hola")
+	testingSet.add("que")
+	testingSet.add("tal")
+	testingSet.add("?")
+	testingSet.display()
+	testingSet.add("¿")
+	testingSet.display()
 }
