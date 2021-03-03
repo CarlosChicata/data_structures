31032dd3c4c7966804a17604c720df213e3eab09
CarlosChicata <carlos.fernando.chicata@gmail.com>
2021-03-03 06:45:28 -0500

    feat: add, display and belong method of dst

--- set/dst.go
+++ set/dst.go
@@ -5,13 +5,13 @@ Purpose:
 Version: 1.0
 
 Methods:
-	- add [ ] : add element in set if not exists
+	- add [x] : add element in set if not exists
 	- remove [ ] : remove element in set if exists
 	- intersect [ ] : all elements will intersect in two sets
-	- belong [ ] : element belong this set
+	- belong [x] : element belong this set
 	- difference [ ] : all elemnet will not interset in two sets
 	- len [ ] : count all elements of universe set
-	- display [ ] : display all element in universe set
+	- display [x] : display all element in universe set
 */
 
 package main
@@ -23,7 +23,41 @@ type duplicatedSet struct {
 	universe []string
 }
 
+/// verify if element inside set
+func (S *duplicatedSet) belong (element string) bool {
+	isBelong := false
+	for _, value := range S.universe {
+		if element == value {
+			isBelong = true
+			break;
+		}
+	}
+	return isBelong
+}
+
+/// display all element in set
+func (S *duplicatedSet) display () {
+	var msg string
+	for _, value := range S.universe {
+		msg = fmt.Sprintf("value is: %s", value)
+		fmt.Println(msg)
+	}
+	fmt.Println("------END------")
+}
+
+/// add element in set if not exists
+func (S *duplicatedSet) add (element string) bool {
+	S.universe = append(S.universe, element)
+	return true
+}
+
 
 func main(){
 	testingSet := duplicatedSet{}
+	testingSet.add("¿")
+	testingSet.add("hola")
+	testingSet.add("que")
+	testingSet.add("tal")
+	testingSet.add("?")
+	testingSet.display()
 }
