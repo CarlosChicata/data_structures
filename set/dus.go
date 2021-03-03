b17d353e12fbf76fd77af34234731bc384c2797e
CarlosChicata <carlos.fernando.chicata@gmail.com>
2021-03-03 07:11:05 -0500

    feat: add disjoint-union set alias dus file to project

---
+++ set/dus.go
@@ -0,0 +1,28 @@
+/*
+Purpose:
+	implement my version of disjoint-union set.
+
+Version: 1.0
+
+Methods:
+	- add [ ] : add element in set if not exists
+	- remove [ ] : remove element in set if exists
+	- intersect [ ] : all elements will intersect in two sets
+	- belong [ ] : element belong this set
+	- difference [ ] : all elemnet will not interset in two sets
+	- len [ ] : count all elements of universe set
+	- display [ ] : display all element in universe set
+*/
+
+package main
+
+import "fmt"
+
+
+type DisjointUnion struct {
+	universe []string
+}
+
+func main(){
+
+}
