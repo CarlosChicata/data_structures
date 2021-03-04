0ffbd752fbda2f7b640982066f4514eeb87d14c4
CarlosChicata <carlos.fernando.chicata@gmail.com>
2021-03-04 06:30:00 -0500

    docs: update docstring of dus file

--- set/dus.go
+++ set/dus.go
@@ -5,13 +5,14 @@ Purpose:
 Version: 1.0
 
 Methods:
-	- add [ ] : add element in set if not exists
-	- remove [ ] : remove element in set if exists
-	- intersect [ ] : all elements will intersect in two sets
-	- belong [ ] : element belong this set
-	- difference [ ] : all elemnet will not interset in two sets
+	- add [ ] : add element in universe if not exists
+	- belong [ ] : element belong this universe
 	- len [ ] : count all elements of universe set
 	- display [ ] : display all element in universe set
+	- union [ ] : merge two set
+	- parentIn [ ] : get parent of set
+	- sizeIn [ ] : get size of set
+	- sizeSet [ ] : get number of set in universe.
 */
 
 package main
@@ -20,7 +21,7 @@ import "fmt"
 
 
 type DisjointUnion struct {
-	universe []string
+	universe [string]string
 }
 
 func main(){
