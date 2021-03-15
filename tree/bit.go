c4baa5e3da856775e2f994893bcfd2950325c6f8
CarlosChicata <carlos.fernando.chicata@gmail.com>
2021-03-15 07:12:42 -0500

    feat: add 'add' method to bit

--- tree/bit.go
+++ tree/bit.go
@@ -14,6 +14,7 @@ Methods:
 
 package main
 
+
 import "fmt"
 
 
@@ -29,7 +30,28 @@ type myBinaryTree struct {
 	comparing func(string, string) bool
 }
 
+func (T *myBinaryTree) add (value string) {
+	currentNode := T.head
+	
+	for currentNode != nil {
+		if T.comparing(value, currentNode.value) {
+			currentNode = currentNode.right
+		}else{
+			currentNode = currentNode.left
+		}
+	}
+
+	T.length++
+	currentNode = &myNode{value: value, left: nil, right: nil}
+}
+
+
 
 func main(){
-	
+	testingTree := myBinaryTree{length: 0, comparing: func(a string, b string) bool { return a < b}}
+	fmt.Println(testingTree)
+	testingTree.add("hola")
+	testingTree.add("que")
+	testingTree.add("tal")
+	testingTree.add("?")
 }
